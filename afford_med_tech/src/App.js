5b76099e73f5282764d32c344923e3c34b7eb0ca
KeerthivasanBP <keerthivasanbp@gmail.com>
2024-06-14 13:13:04 +0530

    new commit

---
+++ afford_med_tech/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import average from './average';
+
+function App() {
+  return (
+    <div className="App">
+      <route>X</route>
+    </div>
+  );
+}
+
+export default App;
